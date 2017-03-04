363b682498949dbc07442e2cc1d98f166c58f03f
Arnedo5 <arnedo555@gmail.com>
2017-03-04 17:06:50 +0100

    Versio 1 | Portada + empresa OK

---
+++ Empresa/js/script.js
@@ -0,0 +1,58 @@
+//BURGER MENU | MENU
+$(document).ready(function ($) {
+	var $L = 1200,
+		$menu_navegacio = $('#botoneraMenu'),
+		$icona_hamburguesa = $('#menuBurger'),
+		$sombrejat = $('#cd-shadow-layer');
+
+	//Obrir el menu lateral | esquerra | en mobil
+	$icona_hamburguesa.on('click', function (event) {
+		event.preventDefault();
+		obrirMenuLateral($menu_navegacio, $sombrejat, $('body'));
+	});
+
+	//Mostrar la botonera de menus | esquerra |
+	$(window).on('resize', function () {
+		if ($(window).width() >= $L && $menu_navegacio.hasClass('botoneraLateral')) {
+			$menu_navegacio.removeClass('botoneraLateral');
+			$sombrejat.removeClass('is-visible');
+			$('body').removeClass('overflow-hidden');
+		}
+
+	});
+
+	//Obrir el menu lateral | esquerra | en mobil
+	function obrirMenuLateral($lateral_panel, $background_layer, $body) {
+		if ($lateral_panel.hasClass('botoneraLateral')) {
+			$lateral_panel.removeClass('botoneraLateral');
+			$background_layer.removeClass('is-visible');
+			$body.removeClass('overflow-hidden');
+		} else {
+			$lateral_panel.addClass('botoneraLateral');
+			$background_layer.addClass('is-visible');
+			$body.addClass('overflow-hidden');
+		}
+	}
+	//Mostrar la botonera de menus | esquerra |
+    /*
+	function botonsMenu( $navigation, $MQ) {
+		if ( $(window).width() >= $MQ ) {
+			$navigation.detach();
+			$navigation.appendTo('header');
+		} else {
+			$navigation.detach();
+			$navigation.insertAfter('header');
+		}
+	}*/
+});
+
+/* Parallax banner*/
+var s = skrollr.init({
+  smoothScrolling: false,
+  forceHeight: false
+});
+
+/* Efecte targeta al fer hover*/
+$('.card').hover(function(){
+    $(this).toggleClass('flipped');
+});
