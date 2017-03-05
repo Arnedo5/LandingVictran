486f17ec198c1bbbc436c86300ff4248434bcde2
Arnedo5 <arnedo555@gmail.com>
2017-03-05 15:26:45 +0100

    Version 1.2 | Galeria d'imatges mig ok

--- Galeria/js/script.js
+++ Galeria/js/script.js
@@ -45,3 +45,23 @@ $(document).ready(function ($) {
 		}
 	}*/
 });
+
+$(document).ready(function(){
+  
+  $(".slider").slick({
+    autoplay:true,
+    autoplaySpeed:10000,
+    speed:600,
+    slidesToShow:1,
+    slidesToScroll:1,
+    pauseOnHover:false,
+    dots:true,
+    pauseOnDotsHover:true,
+    cssEase:'linear',
+   // fade:true,
+    draggable:false,
+    prevArrow:'<button class="PrevArrow"><i class="icon-circle-left"></i></button>',
+    nextArrow:'<button class="NextArrow"><i class="icon-circle-right"></button>', 
+  });
+  
+})
